ac7ad3b92f38cd98077b69f56ed3ef3f359b4159
Rafael Garcia <rafaellevis07@gmail.com>
2022-06-28 18:44:51 -0700

    CRUD operations and routes done

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,13 +1,44 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :post_params, only: [:create, :update]
     def index
         @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def new
         @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+
+    def edit
+    end
+
+    def update
+        @post.update(post_params)
+        redirect_to @post
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to posts_path
+    end
+
+
+    
+    private
+    def set_post 
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        post_params = params.require(:post).permit(:title, :author, :content)
     end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get '/posts', to:'posts#index'
   get 'posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
   get '/posts/:id', to: 'posts#show', as: 'post'
+  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
