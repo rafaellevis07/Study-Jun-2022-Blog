6e189a3a23a3cbef5a242f8bedb8d78ddf122bbd
Rafael Garcia <rafaellevis07@gmail.com>
2022-06-29 21:43:26 -0700

    Associating Comments to Posts

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_170415) do
+ActiveRecord::Schema.define(version: 2022_06_29_214649) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
