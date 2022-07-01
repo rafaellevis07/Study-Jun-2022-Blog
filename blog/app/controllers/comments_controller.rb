d8ac586facd3a8242658f0a0ded1a34346e75d00
Rafael Garcia <rafaellevis07@gmail.com>
2022-06-30 18:06:49 -0700

    Comments associated with Posts

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,20 +1,41 @@
 class CommentsController < ApplicationController
-
+  before_action :set_post
+  before_action :set_comment, except: [:create]
 
       # No `new` action because form is provided by PostsController#show
+      
   def create
-    @post = Post.find(params[:post_id])
     # Create associated model
     @comment = @post.comments.create(comment_params)
-    # We want to show the comment in the context of the Post
+    # It shows the comment in the context of the Post
+    redirect_to @post
+  end
+
+  def edit
+  end
+
+  def update
+    @comment.update(comment_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @comment.destroy
     redirect_to @post
   end
 
 
   private
   def comment_params
-    params.require(:comment).permit(:name, :content)
+    params.require(:comment).permit(:author, :content)
   end
-end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
 end
